da7a75d3a4d41526cbffd06d443b203076563ba8
duda ִ <madudstan@gmail.com>
2026-03-22 19:25:38 -0300

    primeiro commit

---
+++ src/main/java/com/aep/rotalivre/RotalivreApplication.java
@@ -0,0 +1,13 @@
+package com.aep.rotalivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotalivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotalivreApplication.class, args);
+	}
+
+}
